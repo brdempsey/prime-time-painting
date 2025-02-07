22f27fb530a067532188f40df7951b1f23e19ede
brdempsey <brdempsey5@gmail.com>
2025-02-07 14:48:32 -0600

    removed Image import from src > app > page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import HeroSection from "./components/hero-section";
 import ServicesSection from "./components/services-section";
 import GoalSection from "./components/goal-section";
